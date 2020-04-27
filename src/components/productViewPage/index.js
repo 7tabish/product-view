3d670b675e97d1fd535602d2c3eef53e990ca9e6
asimhyff <asimhyff@users.noreply.github.com>
2020-04-27 16:05:27 +0500

    icons added

--- src/components/productViewPage/index.js
+++ src/components/productViewPage/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 import ImageSlider from './imgView/imgSlider'
 import HorizontalSlider from './imgView/HorizontalSlider'
-
+//overview
+import FeatureIcons from './overview/featureIcons'
+//detail
 import MinorDetails from './productMinorDetails/minorDetails'
 
 export default () => {
     return (
+        <>
         <div className="row">
             <section className="col-xs-12 col-sm-12 col-md-8 col-lg-8">
                 <ImageSlider />
@@ -16,6 +19,15 @@ export default () => {
             <section className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
                 <MinorDetails />
             </section>
-        </div>
+            </div>
+
+
+            <div className="row">
+            <section className="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+                <FeatureIcons />
+            </section>
+            
+            </div>
+        </>
     )
 }
--- src/components/productViewPage/overview/featureIcons.js
+++ src/components/productViewPage/overview/featureIcons.js
@@ -1,9 +1,52 @@
 import React from 'react'
 
+//icons
+import DriveEtaIcon from '@material-ui/icons/DriveEta';
+
+//css
+let flexStyle = {
+    display: 'flex',
+    flex:''
+}
+
 export default () => {
     return (
-        <div>
-            
+        <div className="p-2">
+            <h3>Overview</h3>
+            <div className="row" style={{display:'flex' , flexWrap:'wrap'}}>
+            <section className=" p-2 m-3" style={flexStyle}>
+                <DriveEtaIcon  className="mr-1" />
+                <h6>4 Wheel Drive</h6>
+            </section>
+            <section className=" p-2 m-3" style={flexStyle}>
+                <DriveEtaIcon  className="mr-1" />
+                <h6>4 Wheel Drive</h6>
+            </section>
+            <section className="p-2 m-3" style={flexStyle}>
+                <DriveEtaIcon  className="mr-1" />
+                <h6>4 Wheel Drive</h6>
+            </section>
+            <section className="p-2 m-3" style={flexStyle}>
+                <DriveEtaIcon  className="mr-1" />
+                <h6>4 Wheel Drive</h6>
+                </section>
+                <section className="p-2 m-3" style={flexStyle}>
+                <DriveEtaIcon  className="mr-1" />
+                <h6>4 Wheel Drive</h6>
+            </section>
+            <section className="p-2 m-3" style={flexStyle}>
+                <DriveEtaIcon  className="mr-1" />
+                <h6>4 Wheel Drive</h6>
+            </section>
+            <section className="p-2 m-3" style={flexStyle}>
+                <DriveEtaIcon  className="mr-1" />
+                <h6>4 Wheel Drive</h6>
+            </section>
+            <section className="p-2 m-3" style={flexStyle}>
+                <DriveEtaIcon  className="mr-1" />
+                <h6>4 Wheel Drive</h6>
+            </section>
+            </div>
         </div>
     )
 }
